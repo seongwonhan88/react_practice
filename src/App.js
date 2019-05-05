254860073f9ac8b038087eccf7a6f5ed8ca322d7
Seongwon Han <seongwonhan88@gmail.com>
2019-05-05 20:59:40 +0900

    event listener

--- src/App.js
+++ src/App.js
@@ -7,16 +7,23 @@ class App extends Component {
     persons: [
       {name: 'Seongwon', age: 31},
       {name: 'Angela', age: 32},
+      {name: 'child', age: 1},
     ]
   }
+
+  switchNameHandler = () => {
+    console.log('Was clicked')
+  }
+
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>child value here</Person>
       </div>
     );
   }
