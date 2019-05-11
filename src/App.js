dee8a0285185693ce25b53571bdbd3d0ad5920f2
Seongwon Han <seongwonhan88@gmail.com>
2019-05-11 23:59:21 +0900

    js render with loop

--- src/App.js
+++ src/App.js
@@ -51,19 +51,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age} />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.switchNameHandler.bind(this, 'BRAHHH')}
-            changed={this.nameChangedHandler}
-          >
-            child value here</Person>
+          {this.state.persons.map(person => {
+            return <Person 
+            name={person.name} 
+            age={person.age} />
+          })}
+        
         </div>
       );
     }
