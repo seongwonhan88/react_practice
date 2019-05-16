2c228602bfcf0daeed39c9723b1be86bf70df029
Seongwon Han <seongwonhan88@gmail.com>
2019-05-16 23:17:50 +0900

    dynamic style passing to js

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends Component {
   render() {
     const style = {
       backgroundColor: 'white',
+      color: 'green',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -71,6 +72,7 @@ class App extends Component {
         
         </div>
       );
+      style.backgroundColor = 'red';
     }
 
     return (
