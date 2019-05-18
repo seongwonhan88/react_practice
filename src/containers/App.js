809807fd281e4c7d085d6af8bad7add0edb0e2c4
Seongwon Han <seongwonhan88@gmail.com>
2019-05-18 14:52:00 +0900

    divided components containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -47,7 +47,6 @@ class App extends Component {
   }
   render() {
 
-
     let persons = null;
     let btnClass = '';
 
@@ -55,11 +54,11 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <Person 
+              key={person.id}
               name={person.name}
               age={person.age}
               click={() => this.deletePersonHandler(index)}
-              key={person.id}
               changed={(event) => this.nameChangedHandler(event, person.id)}
             />
           })}
