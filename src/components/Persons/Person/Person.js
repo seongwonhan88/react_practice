4c38226fb234e78bd93fce9a1cb7acfb0d5cb183
Seongwon Han <seongwonhan88@gmail.com>
2019-05-18 21:31:06 +0900

    multiple element render using aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
-import classes from './Person.css'
+import React, { Component } from 'react';
+import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
 
 
-    render(){
+    render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type='text' onChange={this.props.changed} value={this.props.name} />
-            </div>
-        )
+            <Aux>
+                <p key="" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+                <p key="">{this.props.children}</p>,
+                <input key="" type='text' onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+        );
     }
 }
 
